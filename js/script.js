48a656bfe51aeceabcbf8e145418fb049c5c37df
mexwebdev21 <encryptedhydra2020@outlook.com>
2022-02-02 03:16:21 +0700

    All pages are working in desktop and tablet mode, although there are a few quirks that need to be worked out.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ const headerEL = document.querySelector(".header");
 btnNavEL.addEventListener("click", function () {
   headerEL.classList.toggle("nav-open");
 });
+
+///////////////////////////////////////////////////////////
+// Leaflet.js map
+const mapCanada = L.map("mapCanada").setView([43.7093, -79.2449], 15);
+
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(mapCanada);
+
+// 12/-33.1168/151.6566  australia
+
+// ap=15/53.7329/-1.3337   Uk
