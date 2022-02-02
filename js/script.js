5bf208ff769819eb96cde8a368812d7126c0a497
mexwebdev21 <encryptedhydra2020@outlook.com>
2022-02-02 17:28:37 +0700

    All pages have been optimized for all breakpoints. Maps work, but only on one instance; still figuring a way to make 3 maps appear on the same page. Form validation is ongoing.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ btnNavEL.addEventListener("click", function () {
 
 ///////////////////////////////////////////////////////////
 // Leaflet.js map
-const mapCanada = L.map("mapCanada").setView([43.7093, -79.2449], 15);
+
+const mapCanada = L.map("mapCanada").setView([43.7093, -79.2449], 14);
 
 const attribution =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
@@ -22,3 +23,35 @@ tiles.addTo(mapCanada);
 // 12/-33.1168/151.6566  australia
 
 // ap=15/53.7329/-1.3337   Uk
+
+///////////////////////////////////////////////////////////
+// CONTACT FORM
+
+// const contactForm = document.getElementById("contact-form");
+// const names = document.getElementById("name");
+// const email = document.getElementById("email");
+// const phone = document.getElementById("phone");
+// const messages = document.getElementById("message");
+
+// // Show input error message
+// function showError(input, message) {
+//   const formControl = input.parentElement;
+//   formControl.className = "contact-form__group error";
+//   const small = formControl.querySelector("contact-form__error");
+//   small.innerText = message;
+// }
+
+// function checkRequired(inputArr) {
+//   inputArr.forEach(function (input) {
+//     if (input.value.trim() === "") {
+//       showError(input, "is required");
+//     }
+//   });
+// }
+
+// // Event Listeners
+// contactForm.addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   checkRequired([names, email, phone, messages]);
+// });
